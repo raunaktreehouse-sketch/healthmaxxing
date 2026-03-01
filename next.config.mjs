77839859367d2a666fdd31774291b03fa41c4613
raunaktreehouse-sketch <raunaktreehouse@gmail.com>
2026-03-01 12:46:10 +0530

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'utfs.io' },
+      { protocol: 'https', hostname: 'uploadthing.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
+  },
+  experimental: {
+    serverActions: { allowedOrigins: ['localhost:3000', 'healthmaxxing.org'] }
+  }
+};
+
+export default nextConfig;
