f9697f5a351efe29055d53e05fcc2ffa80bed4b5
raunaktreehouse-sketch <raunaktreehouse@gmail.com>
2026-03-01 13:16:45 +0530

    Add/Update src/lib/utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,27 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+import { formatDistanceToNow, format } from "date-fns"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: Date | string) {
+  return format(new Date(date), "MMM d, yyyy")
+}
+
+export function formatRelativeTime(date: Date | string) {
+  return formatDistanceToNow(new Date(date), { addSuffix: true })
+}
+
+export function slugify(text: string) {
+  return text
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+}
+
+export function truncate(str: string, length: number) {
+  return str.length > length ? str.substring(0, length) + '...' : str
+}
